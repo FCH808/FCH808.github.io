3daf05016f7c6a3992ab2f09f164700cbcfe292b
FCH808 <fch808@gmail.com>
2015-03-27 15:17:51 +0900

    Add Intro to Machine Learning files, final project python scripts, and python notebooks with completed lesson exercises.

---
+++ Intro to Machine Learning/notebooks/scripts/ClassifyNB.py
@@ -0,0 +1,9 @@
+def classify(features_train, labels_train):   
+    ### import the sklearn module for GaussianNB
+    ### create classifier
+    ### fit the classifier on the training features and labels
+    ### return the fit classifier
+    from sklearn.naive_bayes import GaussianNB
+    clf = GaussianNB()
+    clf.fit(features_train, labels_train)
+    return clf
